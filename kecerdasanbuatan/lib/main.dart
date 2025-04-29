e0bc497c55687aec17e0a46cae0ea09a9a100513
Jennifer Verty 2327250040 <jenniferverty0605@gmail.com>
2025-04-29 11:38:51 +0700

    jenni

--- kecerdasanbuatan/lib/main.dart
+++ kecerdasanbuatan/lib/main.dart
@@ -1,41 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:kecerdasanbuatan/screens/home_screen.dart';
-import 'firebase_options.dart';
+import 'package:kecerdasanbuatan/screens/home_screen.dart'; 
+import 'firebase_options.dart'; 
 
-<<<<<<< HEAD
-void main() {
-  runApp(SistemPakarTHT());
-}
-
-class SistemPakarTHT extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Sistem Pakar THT',
-      theme: ThemeData(primarySwatch: Colors.blue),
-=======
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized(); // <- WAJIB
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+    options: DefaultFirebaseOptions.currentPlatform, 
   );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'THT Checker',
+      title: 'Diagnosa THT',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepPurple,
       ),
       home: const HomeScreen(),
->>>>>>> ec7d69aeaec8205c3f8bbdd83271267eceafb85c
     );
   }
-}      
-
+}
