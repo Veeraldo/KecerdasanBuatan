69a352c4548c5281a47b74ca5e788c1ccf3674d5
Veeraldo <Veraldo24092005@gmail.com>
2025-04-29 11:34:49 +0700

    push

--- kecerdasanbuatan/lib/main.dart
+++ kecerdasanbuatan/lib/main.dart
@@ -3,9 +3,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:kecerdasanbuatan/screens/home_screen.dart';
 import 'firebase_options.dart';
 
-<<<<<<< HEAD
-void main() {
-  runApp(SistemPakarTHT());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Memastikan binding widget terinisialisasi
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform, // Menggunakan FirebaseOptions sesuai platform
+  );
+  runApp(SistemPakarTHT()); // Menjalankan aplikasi setelah Firebase diinisialisasi
 }
 
 class SistemPakarTHT extends StatelessWidget {
@@ -14,28 +17,7 @@ class SistemPakarTHT extends StatelessWidget {
     return MaterialApp(
       title: 'Sistem Pakar THT',
       theme: ThemeData(primarySwatch: Colors.blue),
-=======
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized(); // <- WAJIB
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'THT Checker',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomeScreen(),
->>>>>>> ec7d69aeaec8205c3f8bbdd83271267eceafb85c
+      home: const HomeScreen(), // Mengarahkan ke HomeScreen setelah aplikasi berjalan
     );
   }
-}      
-
+}
