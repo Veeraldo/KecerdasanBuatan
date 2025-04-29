9428ef0086f6549c2682b96e6fbed54b0ff4daf7
Veeraldo <Veraldo24092005@gmail.com>
2025-04-29 11:06:15 +0700

    push

--- kecerdasanbuatan/lib/main.dart
+++ kecerdasanbuatan/lib/main.dart
@@ -1,20 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:kecerdasanbuatan/screens/home_screen.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform, 
+  );
+  runApp(const HomeScreen());
 }
